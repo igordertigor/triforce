2da57a6f0e1549e51debd9f1fe99fd2335f48400
Valentin Haenel <valentin@haenel.co>
2016-12-15 22:09:53 +0100

    some more tests, ugly boy

---
+++ src/unittest/python/main_tests.py
@@ -0,0 +1,22 @@
+import unittest
+
+from triforce import Virtualenv, parse_venv
+
+
+class TestParseVirtualEnv(unittest.TestCase):
+
+    def test_parse_venv(self):
+        input_ = {'ANY_PROGRAM': {}}
+        expected = Virtualenv('ANY_VENV',
+                              urls=['ANY_PROGRAM'],
+                              symlinks=['ANY_PROGRAM'])
+        received = parse_venv('ANY_VENV', input_)
+        self.assertEqual(expected, received)
+
+    def test_parse_venv_url(self):
+        input_ = {'ANY_PROGRAM': {'url': 'ANY_URL'}}
+        expected = Virtualenv('ANY_VENV',
+                              urls=['ANY_URL'],
+                              symlinks=['ANY_PROGRAM'])
+        received = parse_venv('ANY_VENV', input_)
+        self.assertEqual(expected, received)
